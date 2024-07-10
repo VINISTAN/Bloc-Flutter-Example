cf9be285c4e1b3a4fa6edb5c0d1e49899618548f
vinistanvini464@gmail.com <vinistanvini464@gmail.com>
2024-07-10 13:36:23 +0530

    Material App Title changes

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Bloc',
+      title: 'Bloc Example',
       color: Colors.blueGrey,
       debugShowCheckedModeBanner: false,
       home: Homepage(),
